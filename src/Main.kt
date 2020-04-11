e4827f91302250aeb53360cd15e236f90a2a5c92
Anjar Harimurti <acool90@yahoo.co.id>
2020-04-11 11:02:16 +0700

    Tambah fitur baru (Player vs Computer)

--- src/Main.kt
+++ src/Main.kt
@@ -1,65 +1,55 @@
 class Main {
-    companion object{
+    companion object : iCallback{
         @JvmStatic
         fun main(args: Array<String>) {
             var exit = false
+            var pilih : String
             do{
                 println("Game Guntung Batu Kertas Terminal\n")
                 var namaPlayer : String
                 var gtbPlayer : String
 
+                println("Menu : ")
+                println("1. Player vs Player ")
+                println("2. Player vs Computer ")
+                print("Pilihan : ")
+                pilih = readLine().toString()
+
                 print("Masukkan Nama Player 1 : ")
                 namaPlayer = readLine().toString()
                 print("Gunting / Batu / Kertas: ")
-                gtbPlayer = readLine().toString()
+                gtbPlayer = readLine().toString().toLowerCase()
                 val Player1 = Player(namaPlayer, gtbPlayer)
-
-                print("Masukkan Nama Player 2 : ")
-                namaPlayer = readLine().toString()
-                print("Gunting / Batu / Kertas: ")
-                gtbPlayer = readLine().toString()
-                val Player2 = Player(namaPlayer, gtbPlayer)
-
-                if(Player1.gtb.equals("Gunting") || Player1.gtb.equals("gunting")){
-                    if (Player2.gtb.equals("Gunting") || Player2.gtb.equals("gunting")){
-                        println("SERI")
-                    }else if(Player2.gtb.equals("Batu") || Player2.gtb.equals("batu")){
-                        println("Pemenang adalah ${Player2.name}")
-                    }else if(Player2.gtb.equals("Kertas") || Player2.gtb.equals("kertas")){
-                        println("Pemenang adalah ${Player1.name}")
-                    }else{
-                        println("${Player2.name} salah input")
+                var Player2 = Player("", "")
+                when(pilih){
+                    "1" -> {
+                        print("Masukkan Nama Player 2 : ")
+                        namaPlayer = readLine().toString()
+                        print("Gunting / Batu / Kertas: ")
+                        gtbPlayer = readLine().toString().toLowerCase()
+                        Player(namaPlayer, gtbPlayer)
+                        Player2 = Player(namaPlayer, gtbPlayer)
                     }
-                } else if(Player1.gtb.equals("Batu") || Player1.gtb.equals("batu")){
-                    if (Player2.gtb.equals("Gunting") || Player2.gtb.equals("gunting")){
-                        println("Pemenang adalah ${Player1.name}")
-                    }else if(Player2.gtb.equals("Batu") || Player2.gtb.equals("batu")){
-                        println("SERI")
-                    }else if(Player2.gtb.equals("Kertas") || Player2.gtb.equals("kertas")){
-                        println("Pemenang adalah ${Player2.name}")
-                    }else{
-                        println("${Player2.name} salah input")
+                    "2" -> {
+                        val list = mutableListOf<String>("Gunting", "Batu", "Kertas")
+                        Player2 = Player("Computer", list.random().toLowerCase())
                     }
-                } else if (Player1.gtb.equals("Kertas") || Player1.gtb.equals("kertas")){
-                    if (Player2.gtb.equals("Gunting") || Player2.gtb.equals("gunting")){
-                        println("Pemenang adalah ${Player2.name}")
-                    }else if(Player2.gtb.equals("Batu") || Player2.gtb.equals("batu")){
-                        println("Pemenang adalah ${Player1.name}")
-                    }else if(Player2.gtb.equals("Kertas") || Player2.gtb.equals("kertas")){
-                        println("SERI")
-                    }else{
-                        println("${Player2.name} salah input")
-                    }
-                } else{
-                    println("${Player1.name} salah input")
                 }
+
+                val controller = Controller(Player1,Player2,this)
+                controller.checkWinner()
+
                 print("Coba Lagi (y/n): ")
-                var lagi = readLine().toString()
+                val lagi = readLine().toString()
                 if (lagi.equals("n") || lagi.equals("N")){
                     exit = true
                     println("Terima Kasih...")
                 }
             }while (!exit)
+        }
+
+        override fun showWinner(winner: String) {
+            println("Pemenangnya adalah $winner")
         }
     }
 }
--- src/Player.kt
+++ src/Player.kt
@@ -1,8 +1,17 @@
 class Player(name : String, gtb : String) {
     val name : String
     val gtb : String
+
     init {
         this.name = name
         this.gtb = gtb
+    }
+
+    fun getNama() : String{
+        return name
+    }
+
+    fun getGTB() : String{
+        return gtb
     }
 }
